47870037d8e320a7d3c7f19904a46368e0f6e5e5
Miguel Martelo <miguemartelo1031@gmail.com>
2021-05-18 23:18:30 -0500

    Organize products module.

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
 
-@Module({})
+import { CategoriesController } from './controllers/categories.controller';
+import { ProductsController } from './controllers/products.controller';
+import { ProductsService } from './services/products.service';
+@Module({
+  controllers: [ProductsController, CategoriesController],
+  providers: [ProductsService],
+})
 export class ProductsModule {}
