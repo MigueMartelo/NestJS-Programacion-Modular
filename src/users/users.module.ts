0b5365276af4440233ccbc56bde80b88a5ec6192
Miguel Martelo <miguemartelo1031@gmail.com>
2021-05-26 22:47:37 -0500

    [Add]: Complete users module.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CustomersService } from './services/customers.service';
+import { UsersService } from './services/users.service';
+import { UsersController } from './controllers/users.controller';
+import { CustomersController } from './controllers/customers.controller';
 
-@Module({})
+@Module({
+  providers: [CustomersService, UsersService],
+  controllers: [UsersController, CustomersController],
+})
 export class UsersModule {}
